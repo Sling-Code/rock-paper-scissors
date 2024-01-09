2408a4f605a2b67236b57cc6cbf508b34900385f
Sling-Code <146631260+Sling-Code@users.noreply.github.com>
2024-01-09 05:09:15 -0600

    created function to play through a single round

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function getComputerChoice(options) {
-    let index = Math.floor(Math.random() * 3);
+    let index = Math.floor(Math.random() * options.length);
     return options[index];
 }
 
 let options = ["Rock", "Paper", "Scissors"];
+
+//grabs a choice from the player and the computer and determine win, lose, or draw
+function playRound (playerSelection, computerSelection) {
+    if ((playerSelection === "rock" && computerSelection === "scissors") 
+    || (playerSelection === "paper" && computerSelection === "rock")
+    || (playerSelection === "scissors" && computerSelection === "paper")) {
+        return "You Won!";
+    } else if ( playerSelection === computerSelection) {
+        return "It's a Draw!";
+    } else {
+        return "You Lost!";
+    }
+}
+
+
+let playerSelection = prompt("Choose Rock, Paper or Scissors");
+let computerSelection = getComputerChoice(options);
+    
