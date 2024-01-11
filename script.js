88317041f53dbd81df577398f1c112e3c3026d6c
Sling-Code <146631260+Sling-Code@users.noreply.github.com>
2024-01-11 04:33:53 -0600

    added a scoring system to tally wins and losses and announce the result after the game function is complete

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-function getComputerChoice(options) {
-    let index = Math.floor(Math.random() * options.length);
-    return options[index];
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 let options = ["rock", "paper", "scissors"];
 
+let playerScore = 0;
+let computerScore = 0;
 
-let computerSelection = getComputerChoice(["rock", "paper", "scissors"]);
-
-
-//grabs a choice from the player and the computer and determine win, lose, or draw
+//grabs a choice from the player and the computer and determine win, lose, or draw and keeps score
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "scissors") 
-    || (playerSelection === "paper" && computerSelection === "rock")
-    || (playerSelection === "scissors" && computerSelection === "paper")) {
-        return "You Won!";
-    } else if ( playerSelection === computerSelection) {
-        return "It's a Draw!";
+    if ((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") 
+    || (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock")
+    || (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper")) {
+        console.log("You Won the Round!");
+        playerScore++;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        console.log("It's a Draw!");
     } else {
-        return "You Lost!";
+        console.log("You Lost the Round!");
+        computerScore++;
     }
 }
 
+//gets input from the user and computer and plays through five iterations of the playRound function
 function game() {
     for (i = 1 ; i <= 5 ; i++) {
-    let playerSelection = prompt("Choose Rock, Paper or Scissors");  
-    getComputerChoice(options);
-    console.log(playRound(playerSelection, computerSelection));
+    let playerSelection = prompt("Choose Rock, Paper or Scissors"); 
+    let computerSelection = getComputerChoice();    
     console.log(playerSelection, computerSelection)
+
+    result = playRound(playerSelection, computerSelection);    
+    }
+}  
+
+game();
+
+//tallies up wins and losses and announces the result
+function final() {
+    if (playerScore > computerScore) {
+        console.log("You Won the Game!");
+    } else if (playerScore === computerScore) {
+        console.log("It's a Tie!");
+    } else if (playerScore < computerScore) {
+        console.log("You Lost the Game!")
     }
 }
-    
-game();
+
+final();
